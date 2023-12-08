31ef043f3d4e3992c24acebbd1d12044516806c2
Vinicius <vini383@gmail.com>
2023-12-08 18:31:22 -0300

    feat: added FundamentalConcepts folder

---
+++ FundamentalConcepts/Guid/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GuidProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //Global Unique Identifier
+           var id = Guid.NewGuid();
+
+           Console.WriteLine(id);
+        }
+    }
+}
